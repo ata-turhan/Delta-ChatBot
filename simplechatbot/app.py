b9f068cb2f183b6c44f8b846ef1200f751e1143e
fotino21 <ataturhan21@gmail.com>
2023-05-12 12:48:46 +0300

    feature: command line arguments received

---
+++ simplechatbot/app.py
@@ -0,0 +1,9 @@
+import sys
+
+
+def main():
+    print(f"Command line argument - '{sys.argv[1]}' is received succesfully.")
+
+
+if __name__ == "__main__":
+    main()
--- start.bat
+++
@@ -1,7 +0,0 @@
-chcp 65001
-
-@echo
-
-poetry run streamlit run "C:\Users\NEO\Desktop\Projects\Alpha\app\🤖Welcome.py"
-
-Exit
